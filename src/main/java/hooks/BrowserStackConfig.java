b8b61d78e33361f1b7184486e1609931b86431e3
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-07 21:01:05 +0300

    .

--- src/main/java/hooks/BrowserStackConfig.java
+++ src/main/java/hooks/BrowserStackConfig.java
@@ -12,8 +12,11 @@ import java.net.URL;
 import java.util.HashMap;
 
 public class BrowserStackConfig {
-    private static final String CONFIG_FILE_PATH = ("/src/main/resources/config_properties/BrowserStackCapabilities.properties");
-    private static final String browserStack_ServerURL = ("https://" + PropertiesDataManager.getProperty("username", CONFIG_FILE_PATH) + ":" + PropertiesDataManager.getProperty("accessKey", CONFIG_FILE_PATH) + "@hub-cloud.browserstack.com/wd/hub");
+    private static final String browserStack_ServerURL =
+            ("https://" + PropertiesDataManager.getProperty(
+                    "username", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES) + ":" +
+                    PropertiesDataManager.getProperty("accessKey", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES) +
+                    "@hub-cloud.browserstack.com/wd/hub");
     private static final DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
     private static final HashMap<String, Object> browserstackOptions = new HashMap<>();
     private static AndroidDriver androidDriver;
@@ -35,40 +38,40 @@ public class BrowserStackConfig {
         /**********************************************************************************************************/
         /****************************************Specify the App***************************************************/
         //Specify App
-        desiredCapabilities.setCapability("app", PropertiesDataManager.getProperty("app", CONFIG_FILE_PATH));
+        desiredCapabilities.setCapability("app", PropertiesDataManager.getProperty("app", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /****************************************Select device*****************************************************/
         //Select device
-        desiredCapabilities.setCapability("platformName", PropertiesDataManager.getProperty("platformName", CONFIG_FILE_PATH));
-        desiredCapabilities.setCapability("platformVersion", PropertiesDataManager.getProperty("platformVersion", CONFIG_FILE_PATH));
-        desiredCapabilities.setCapability("deviceName", PropertiesDataManager.getProperty("deviceName", CONFIG_FILE_PATH));
+        desiredCapabilities.setCapability("platformName", PropertiesDataManager.getProperty("platformName", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
+        desiredCapabilities.setCapability("platformVersion", PropertiesDataManager.getProperty("platformVersion", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
+        desiredCapabilities.setCapability("deviceName", PropertiesDataManager.getProperty("deviceName", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /****************************************Select an automation engine***************************************/
         //Select an automation engine
-        desiredCapabilities.setCapability("automationName", PropertiesDataManager.getProperty("automationName", CONFIG_FILE_PATH));
+        desiredCapabilities.setCapability("automationName", PropertiesDataManager.getProperty("automationName", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /****************************************Organize tests****************************************************/
         //Organize tests
-        browserstackOptions.put("projectName", PropertiesDataManager.getProperty("projectName", CONFIG_FILE_PATH));
-        browserstackOptions.put("buildName", PropertiesDataManager.getProperty("buildName", CONFIG_FILE_PATH));
+        browserstackOptions.put("projectName", PropertiesDataManager.getProperty("projectName", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
+        browserstackOptions.put("buildName", PropertiesDataManager.getProperty("buildName", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /****************************************Set debugging options*********************************************/
         //Set debugging options
         //1- Text logs are enabled by default, and cannot be disabled
         //2- Network Logs are disabled by default. To enable network logs use its capability
-        browserstackOptions.put("networkLogs", PropertiesDataManager.getProperty("networkLogs", CONFIG_FILE_PATH));
+        browserstackOptions.put("networkLogs", PropertiesDataManager.getProperty("networkLogs", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         //3- Device logs are enabled by default. To disable device logs use its capability
-        browserstackOptions.put("deviceLogs", PropertiesDataManager.getProperty("deviceLogs", CONFIG_FILE_PATH));
+        browserstackOptions.put("deviceLogs", PropertiesDataManager.getProperty("deviceLogs", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         //4- Appium logs are enabled by default. To disable Appium logs use its capability
-        browserstackOptions.put("appiumLogs", PropertiesDataManager.getProperty("appiumLogs", CONFIG_FILE_PATH));
+        browserstackOptions.put("appiumLogs", PropertiesDataManager.getProperty("appiumLogs", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         //5- Visual logs are disabled by default. To enable visual logs use its capability
-        browserstackOptions.put("debug", PropertiesDataManager.getProperty("debug", CONFIG_FILE_PATH));
+        browserstackOptions.put("debug", PropertiesDataManager.getProperty("debug", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         //6- Video logs are enabled by default. Note that video recording slightly increases the text execution time. To disable video logs use its capability
-        browserstackOptions.put("video", PropertiesDataManager.getProperty("video", CONFIG_FILE_PATH));
+        browserstackOptions.put("video", PropertiesDataManager.getProperty("video", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /****************************************Set Appium version************************************************/
         //Set Appium version
-        browserstackOptions.put("appiumVersion", PropertiesDataManager.getProperty("appiumVersion", CONFIG_FILE_PATH));
+        browserstackOptions.put("appiumVersion", PropertiesDataManager.getProperty("appiumVersion", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES));
         /**********************************************************************************************************/
         /*********************************Set browser stack capability options ************************************/
         //Set bstack:options capabilities
@@ -85,7 +88,7 @@ public class BrowserStackConfig {
     private static void setupYML() {
         try {
             MutableCapabilities capabilities = new UiAutomator2Options();
-            androidDriver = new AndroidDriver(new URL(PropertiesDataManager.getProperty("appiumServerURL", CONFIG_FILE_PATH)), capabilities);
+            androidDriver = new AndroidDriver(new URL(PropertiesDataManager.getProperty("appiumServerURL", PropertiesDataManager.Capability.BROWSERSTACK_CAPABILITIES)), capabilities);
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/hooks/DriverLocalService.java
+++ src/main/java/hooks/DriverLocalService.java
@@ -12,23 +12,22 @@ import java.net.URL;
 
 public class DriverLocalService {
     private static AppiumDriverLocalService appiumService;
-    private static final String CONFIG_FILE_PATH = ("/src/main/resources/config_properties/ExecutionPlatform.properties");
 
     protected static AndroidDriver localServiceInitialization() {
         //Build the Appium service
         AppiumServiceBuilder serviceBuilder = new AppiumServiceBuilder()
                 .withAppiumJS(new File("C:\\Users\\_VOIS\\AppData\\Roaming\\npm\\node_modules\\appium\\build\\lib\\main.js"))
-                .withIPAddress(PropertiesDataManager.getProperty("executionIPAddress", CONFIG_FILE_PATH))
-                .usingPort(Integer.parseInt(PropertiesDataManager.getProperty("executionPort", CONFIG_FILE_PATH)));
+                .withIPAddress(PropertiesDataManager.getProperty("executionIPAddress", PropertiesDataManager.Capability.EXECUTION_CAPABILITIES))
+                .usingPort(Integer.parseInt(PropertiesDataManager.getProperty("executionPort", PropertiesDataManager.Capability.EXECUTION_CAPABILITIES)));
         //Start the server with the builder
         appiumService = AppiumDriverLocalService.buildService(serviceBuilder);
         appiumService.start();
         //Initialize the driver and launch the app
         try {
-            return new AndroidDriver(new URL(PropertiesDataManager.getProperty("appiumServerURL", CONFIG_FILE_PATH)),
+            return new AndroidDriver(new URL(PropertiesDataManager.getProperty("appiumServerURL", PropertiesDataManager.Capability.EXECUTION_CAPABILITIES)),
                     new UiAutomator2Options()
                             .setDeviceName("Pixel 2 XL")
-                            .setApp("C:\\Users\\_VOIS\\Documents\\GitHub\\rahul-appium-automation\\src\\main\\resources\\app_binaries\\ApiDemos-debug.apk"));
+                            .setApp("C:\\Users\\_VOIS\\Documents\\GitHub\\rahul-appium-automation\\src\\main\\resources\\app_binaries\\General-Store.apk"));
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
