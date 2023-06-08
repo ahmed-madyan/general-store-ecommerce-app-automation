e8192bfb902d0218faf40a6069cb05ae21d190f2
Ahmed Madyan <ahmed.madyan@vodafone.com>
2023-06-08 13:19:05 +0300

    Update SignUpTest.java

--- src/test/java/SignUpTest.java
+++ src/test/java/SignUpTest.java
@@ -8,13 +8,13 @@ import hooks.TestBase;
 
 public class SignUpTest extends TestBase {
     private final By country_List = AppiumBy.id("com.androidsample.generalstore:id/spinnerCountry");
-    private final String country = ("//android.widget.TextView[@text='{country}']");
     private final By name_TextFiled = AppiumBy.id("com.androidsample.generalstore:id/nameField");
     private final By letsShop_btn = AppiumBy.id("com.androidsample.generalstore:id/btnLetsShop");
 
     @Test
     public void signUp() {
         MobileGestures.click(country_List);
+        String country = ("//android.widget.TextView[@text='{country}']");
         AppiumActions.scrollIntoView(AppiumBy.xpath(country.replace("{country}", "Egypt")), "Egypt");
         MobileGestures.click(AppiumBy.xpath(country.replace("{country}", "Egypt")));
         ElementActions.sendKeys(name_TextFiled, "Ahmed");
